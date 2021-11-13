1041ab46acb894c8761a7859e892db3bcad2c705
Rasl-Ait <dovshi1208@gmail.com>
2021-11-14 00:01:05 +0300

    refactor: Lists presenter test

--- final-task/MovieApp/MovieAppTests/Presenters/ListsPresenterTest.swift
+++ final-task/MovieApp/MovieAppTests/Presenters/ListsPresenterTest.swift
@@ -8,8 +8,8 @@
 import XCTest
 @testable import MovieApp
 
- class AccountAndListServiceSpy: AccountAndListService {
-
+class AccountAndListServiceSpy: AccountAndListService {
+  
   var responseList: ListResponce?
   var responseNewList: NewListResponce?
   var responseSuccessError: SuccessErrorModel?
@@ -70,7 +70,7 @@ class ListsViewControllerMock: ListsViewInput {
   var isCalledAccount: Bool!
   
   func success(items: [ListModel]) {
-   isCalledFetchLists = true
+    isCalledFetchLists = true
   }
   
   func successCreateList(text: String) {
@@ -103,114 +103,83 @@ class ListsViewControllerMock: ListsViewInput {
 }
 
 class ListsPresenterTest: XCTestCase {
-  private var presenter: ListsPresenter!
-  private var view: ListsViewControllerMock!
-  private var service: AccountAndListServiceSpy!
-  var coredataStack: CoreDataStack!
-  var persistence: StorageProtocol!
-
-  override func setUp() {
-    super.setUp()
-    coredataStack = CoreDataStack(modelName: "Model", storageType: .inMemory)
-    persistence = MoviePersistence(context: coredataStack.managedContext, backgroundContext: coredataStack.managedContext)
-  }
-  
-  override func tearDown() {
-    presenter = nil
-    persistence = nil
-    service = nil
-    coredataStack = nil
-    view = nil
-    super.tearDown()
-  }
   
   func test_getAccountSuccess() {
-    
     let results = getResponce(file: "Account", type: AccountModel.self)
     
-    service = AccountAndListServiceSpy(client: NetworkService(session: URLSession.shared))
-    service.responseAccount = results.item
+    let sut = makeSUT()
+    sut.service.responseAccount = results.item
     
-    view = ListsViewControllerMock()
-    presenter = ListsPresenter(view: view, service: service, persistence: persistence, mediaID: 1)
-    self.presenter.getAccount {
+    sut.presenter.getAccount {
       
     }
-    service.getAccount { [weak self] result in
-      guard let self = self else { return }
+    sut.service.getAccount { result in
       switch result {
       case .success(let item):
         XCTAssertNotNil(item)
       case .failure(let error):
-        self.view.failure(error: error)
+        sut.view.failure(error: error)
       }
     }
   }
   
   func test_getAccountFailure() {
-    service = AccountAndListServiceSpy(client: NetworkService(session: URLSession.shared))
+    let results = getResponce(file: "Account", type: AccountModel.self)
     
-    view = ListsViewControllerMock()
-    presenter = ListsPresenter(view: view, service: service, persistence: persistence, mediaID: 1)
-    self.presenter.getAccount {
+    let sut = makeSUT()
+    sut.service.responseAccount = results.item
+    
+    sut.presenter.getAccount {
       
     }
-    service.getAccount { [weak self] result in
-      guard let self = self else { return }
+    sut.service.getAccount { result in
       switch result {
       case .success(let item):
         XCTAssertNotNil(item)
       case .failure(let error):
-        self.view.failure(error: error)
-        XCTAssertFalse(self.view.isCalledAccount)
+        sut.view.failure(error: error)
+        XCTAssertFalse(sut.view.isCalledAccount)
       }
     }
   }
-
+  
   func test_getListsSuccess() {
     
     let results = getResponce(file: "ListModel", type: ListResponce.self)
     
-    service = AccountAndListServiceSpy(client: NetworkService(session: URLSession.shared))
-    service.responseList = results.item
+    let sut = makeSUT()
+    sut.service.responseList = results.item
     
-    view = ListsViewControllerMock()
-    presenter = ListsPresenter(view: view, service: service, persistence: persistence, mediaID: 1)
-    self.presenter.getLists(state: .noRefresh)
-    service.getLists(1) { [weak self] result in
-      guard let self = self else { return }
+    sut.presenter.getLists(state: .noRefresh)
+    sut.service.getLists(1) { result in
       switch result {
       case .success(let item):
-        self.view.success(items: item.results)
-        XCTAssertTrue(self.view.isCalledFetchLists)
+        sut.view.success(items: item.results)
+        XCTAssertTrue(sut.view.isCalledFetchLists)
       case .failure(let error):
-        self.view.failure(error: error)
+        sut.view.failure(error: error)
       }
     }
   }
   
-func test_getListsFailure() {
-  
-  let results = getResponce(file: "ListModel1", type: ListResponce.self)
-  
-  service = AccountAndListServiceSpy(client: NetworkService(session: URLSession.shared))
-  service.responseList = results.item
-  
-  view = ListsViewControllerMock()
-  presenter = ListsPresenter(view: view, service: service, persistence: persistence, mediaID: 1)
-  self.presenter.getLists(state: .noRefresh)
-  service.getLists(1) { [weak self] result in
-    guard let self = self else { return }
-    switch result {
-    case .success(let item):
-      self.view.success(items: item.results)
-    case .failure(let error):
-      self.view.failure(error: error)
+  func test_getListsFailure() {
+    
+    let results = getResponce(file: "ListModel1", type: ListResponce.self)
+    
+    let sut = makeSUT()
+    sut.service.responseList = results.item
+    
+    sut.presenter.getLists(state: .noRefresh)
+    sut.service.getLists(1) { result in
+      switch result {
+      case .success(let item):
+        sut.view.success(items: item.results)
+        XCTAssertFalse(sut.view.isCalledFetchLists)
+      case .failure(let error):
+        sut.view.failure(error: error)
+      }
     }
   }
-  
-  XCTAssertFalse(view.isCalledFetchLists)
-}
   
   func test_createNewListSuccess() {
     
@@ -219,45 +188,38 @@ func test_getListsFailure() {
                                statusCode: 1,
                                listId: 5861)
     
-    service = AccountAndListServiceSpy(client: NetworkService(session: URLSession.shared))
-    service.responseNewList = list
+    let sut = makeSUT()
+    sut.service.responseNewList = list
     
-    view = ListsViewControllerMock()
-    presenter = ListsPresenter(view: view, service: service, persistence: persistence, mediaID: 1)
-    presenter.createList()
+    sut.presenter.createList()
     let param = NewListParam(name: "List1")
-    presenter.param = param
-    presenter.createList()
-    service.createList(param) { [weak self] result in
-      guard let self = self else { return }
+    sut.presenter.param = param
+    sut.presenter.createList()
+    sut.service.createList(param) { result in
       switch result {
       case .success(let item):
-        self.view.successCreateList(text: item.statusMessage)
-        XCTAssertTrue(self.view.isCalledCreateNewList)
+        sut.view.successCreateList(text: item.statusMessage)
+        XCTAssertTrue(sut.view.isCalledCreateNewList)
       case .failure(let error):
-        self.view.failure(error: error)
+        sut.view.failure(error: error)
       }
     }
   }
   
   func test_createNewListFailure() {
-    service = AccountAndListServiceSpy(client: NetworkService(session: URLSession.shared))
-    
-    view = ListsViewControllerMock()
-    presenter = ListsPresenter(view: view, service: service, persistence: persistence, mediaID: 1)
-    presenter.createList()
+    let sut = makeSUT()
+    sut.presenter.createList()
     let param = NewListParam(name: "List1")
-    presenter.param = param
-    presenter.createList()
-    service.createList(param) { [weak self] result in
-      guard let self = self else { return }
+    sut.presenter.param = param
+    sut.presenter.createList()
+    sut.service.createList(param) { result in
       switch result {
       case .success(let item):
-        self.view.successCreateList(text: item.statusMessage)
-        XCTAssertTrue(self.view.isCalledCreateNewList)
+        sut.view.successCreateList(text: item.statusMessage)
+        XCTAssertTrue(sut.view.isCalledCreateNewList)
       case .failure(let error):
-        self.view.failure(error: error)
-        XCTAssertFalse(self.view.isCalledCreateNewList)
+        sut.view.failure(error: error)
+        XCTAssertFalse(sut.view.isCalledCreateNewList)
       }
     }
   }
@@ -265,207 +227,81 @@ func test_getListsFailure() {
   func test_createMovieToListSuccess() {
     
     let responseSuccessError = SuccessErrorModel(statusCode: 12, statusMessage: "The item/record was updated successfully.")
+    let sut = makeSUT()
+    sut.service.responseSuccessError = responseSuccessError
     
-    service = AccountAndListServiceSpy(client: NetworkService(session: URLSession.shared))
-    service.responseSuccessError = responseSuccessError
-    
-    view = ListsViewControllerMock()
-    presenter = ListsPresenter(view: view, service: service, persistence: persistence, mediaID: 1)
-    presenter.addMovieToList(id: 1, mediaID: 1)
+    sut.presenter.addMovieToList(id: 1, mediaID: 1)
     
     let param = MovieToListParam(mediaID: 1)
     
-    service.movieToList(1, param: param) { [weak self] result in
-      guard let self = self else { return }
+    sut.service.movieToList(1, param: param) { result in
       switch result {
       case .success(let item):
-        self.view.successAddMovieToList(text: item.statusMessage)
-        XCTAssertTrue(self.view.isCalledMovieToList)
+        sut.view.successAddMovieToList(text: item.statusMessage)
+        XCTAssertTrue(sut.view.isCalledMovieToList)
       case .failure(let error):
-        self.view.failure(error: error)
+        sut.view.failure(error: error)
       }
     }
   }
   
   func test_createMovieToListFailure() {
-    service = AccountAndListServiceSpy(client: NetworkService(session: URLSession.shared))
-    
-    view = ListsViewControllerMock()
-    presenter = ListsPresenter(view: view, service: service, persistence: persistence, mediaID: 1)
-    presenter.addMovieToList(id: 1, mediaID: 1)
+    let sut = makeSUT()
+    sut.presenter.addMovieToList(id: 1, mediaID: 1)
     
     let param = MovieToListParam(mediaID: 1)
     
-    service.movieToList(1, param: param) { [weak self] result in
-      guard let self = self else { return }
+    sut.service.movieToList(1, param: param) { result in
       switch result {
       case .success(let item):
-        self.view.successAddMovieToList(text: item.statusMessage)
-        XCTAssertTrue(self.view.isCalledMovieToList)
+        sut.view.successAddMovieToList(text: item.statusMessage)
+        XCTAssertTrue(sut.view.isCalledMovieToList)
       case .failure(let error):
-        self.view.failure(error: error)
-        XCTAssertFalse(self.view.isCalledMovieToList)
+        sut.view.failure(error: error)
+        XCTAssertFalse(sut.view.isCalledMovieToList)
       }
     }
   }
-
+  
   func test_deleteListSuccess() {
     
     let responseSuccessError = SuccessErrorModel(statusCode: 12, statusMessage: "The item/record was updated successfully.")
+    let sut = makeSUT()
+    sut.service.responseSuccessError = responseSuccessError
     
-    service = AccountAndListServiceSpy(client: NetworkService(session: URLSession.shared))
-    service.responseSuccessError = responseSuccessError
-    
-    view = ListsViewControllerMock()
-    presenter = ListsPresenter(view: view, service: service, persistence: persistence, mediaID: 1)
-    presenter.deleteList(id: 1)
-    service.deleteList(1) { [weak self] result in
-      guard let self = self else { return }
+    sut.presenter.deleteList(id: 1)
+    sut.service.deleteList(1) { result in
       switch result {
       case .success(let item):
-        self.view.successDeleteList(text: item.statusMessage)
-        XCTAssertTrue(self.view.isCalledDeleteList)
+        sut.view.successDeleteList(text: item.statusMessage)
+        XCTAssertTrue(sut.view.isCalledDeleteList)
       case .failure(let error):
-        self.view.failure(error: error)
+        sut.view.failure(error: error)
       }
     }
   }
   
   func test_deleteListFailure() {
-    service = AccountAndListServiceSpy(client: NetworkService(session: URLSession.shared))
-    
-    view = ListsViewControllerMock()
-    presenter = ListsPresenter(view: view, service: service, persistence: persistence, mediaID: 1)
-    presenter.deleteList(id: 1)
-    service.deleteList(1) { [weak self] result in
-      guard let self = self else { return }
+    let sut = makeSUT()
+    sut.presenter.deleteList(id: 1)
+    sut.service.deleteList(1) { result in
       switch result {
       case .success(let item):
-        self.view.successDeleteList(text: item.statusMessage)
-        XCTAssertTrue(self.view.isCalledDeleteList)
+        sut.view.successDeleteList(text: item.statusMessage)
+        XCTAssertTrue(sut.view.isCalledDeleteList)
       case .failure(let error):
-        self.view.failure(error: error)
-        XCTAssertFalse(self.view.isCalledDeleteList)
+        sut.view.failure(error: error)
+        XCTAssertFalse(sut.view.isCalledDeleteList)
       }
     }
   }
   
-private func makeSUT() -> (service: AccountAndListService, client: MockHTTPClient) {
-  let httpClient = MockHTTPClient()
-  let sut = AccountAndListService(client: httpClient)
-  return (sut, httpClient)
+  private func makeSUT() -> (service: AccountAndListServiceSpy, presenter: ListsPresenter, view: ListsViewControllerMock) {
+    let service = AccountAndListServiceSpy(client: NetworkService(session: URLSession.shared))
+    let view = ListsViewControllerMock()
+    let coredataStack = CoreDataStack(modelName: "Model", storageType: .inMemory)
+    let persistence = MoviePersistence(context: coredataStack.managedContext, backgroundContext: coredataStack.managedContext)
+    let presenter = ListsPresenter(view: view, service: service, persistence: persistence, mediaID: 1)
+    return (service, presenter, view)
+  }
 }
-}
-
-//
-//class ListsViewControllerMock: ListsViewInput {
-//
-//  var lists: [ListModel] = []
-//  var successCreateList = ""
-//  var successDeleteList = ""
-//
-//  func success(items: [ListModel]) {
-//    lists = items
-//  }
-//
-//  func successCreateList(text: String) {
-//    successCreateList = text
-//  }
-//
-//  func successDeleteList(text: String) {
-//    successDeleteList = text
-//  }
-//
-//  func successAddMovieToList(text: String) {
-//
-//  }
-//
-//  func failure(error: APIError) {
-//
-//  }
-//
-//  func hideIndicator() {
-//
-//  }
-//
-//  func showIndicator() {
-//
-//  }
-//}
-//
-//class ListSPresenterSpy: ListsViewOutput {
-//
-//  weak var view: ListsViewInput?
-//
-//  init(view: ListsViewInput) {
-//    self.view = view
-//  }
-//
-//  var lists: [ListModel] = []
-//
-//  func getLists(state: StateLoad) {
-//    let item = ListModel(description: "",
-//                         favoriteCount: 1,
-//                         id: 1,
-//                         itemCount: 1,
-//                         listType: "movie",
-//                         name: "List 1",
-//                         posterPath: nil,
-//                         movies: nil)
-//    lists.append(item)
-//    view?.success(items: lists)
-//  }
-//
-//  func createList() {
-//    view?.successCreateList(text: "Create list success")
-//
-//  }
-//
-//  func deleteList(id: Int) {
-//    view?.successDeleteList(text: "Delete list success")
-//  }
-//
-//  func didSelectRowAt(list: ListModel) {
-//
-//  }
-//
-//  func addText(name: String) {
-//
-//  }
-//
-//  func pop() {
-//
-//  }
-//}
-//
-//class ListsPresenterTest: XCTestCase {
-//  private var presenter: ListSPresenterSpy!
-//  private var view: ListsViewControllerMock!
-//
-//  override func setUp() {
-//    super.setUp()
-//   view = ListsViewControllerMock()
-//    presenter = ListSPresenterSpy(view: view)
-//  }
-//
-//  override func tearDown() {
-//    presenter = nil
-//    view = nil
-//    super.tearDown()
-//  }
-//
-//  func test_getListsSuccess() {
-//    presenter.getLists(state: .noRefresh)
-//    XCTAssertEqual(view.lists.count, 1)
-//}
-//
-//  func test_createListSuccess() {
-//    presenter.createList()
-//    XCTAssertEqual(view.successCreateList, "Create list success")
-//}
-//
-//  func test_deteleListSuccess() {
-//    presenter.deleteList(id: 1)
-//    XCTAssertEqual(view.successDeleteList, "Delete list success")
-//}
-//}
