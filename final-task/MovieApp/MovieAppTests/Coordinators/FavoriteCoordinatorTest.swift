ba92013366f77daf12f65373acafd280d7392887
Rasl-Ait <dovshi1208@gmail.com>
2021-11-19 09:56:15 +0300

    feat: Favorite Coordinator covered with tests

---
+++ final-task/MovieApp/MovieAppTests/Coordinators/FavoriteCoordinatorTest.swift
@@ -0,0 +1,43 @@
+//
+//  FavoriteCoordinatorTest.swift
+//  MovieAppTests
+//
+//  Created by rasul on 11/19/21.
+//
+
+import XCTest
+@testable import MovieApp
+
+class FavoriteCoordinatorTest: XCTestCase {
+
+  private var favoriteCoordinator: FavoriteCoordinatorProtocol!
+  private var router: RouterMock!
+  private var coordinatorFactory: CoordinatorFactoryMock!
+  private var screenFactory: ScreenFactoryMock!
+  private var tabBarController = TabBarController()
+  
+  override func setUp() {
+    super.setUp()
+    router = RouterMock()
+    screenFactory = ScreenFactoryMock()
+    coordinatorFactory = CoordinatorFactoryMock()
+    favoriteCoordinator = FavoriteCoordinator(router: router, coordinatorFactory: coordinatorFactory, screenFactory: screenFactory, tabBarViewController: TabBarController())
+  }
+  
+  func test_pushMovieDetailVC() {
+    
+    screenFactory.makeMovieDetailScreenHandler = { _, _ in
+      return MovieDetailViewController()
+    }
+  
+    coordinatorFactory.makeMovieDetailCoordinatorHandler = { router, id in
+      return MovieDetailCoordinator(router: router, coordinatorFactory: self.coordinatorFactory, screenFactory: self.screenFactory, movieID: id)
+    }
+    
+    favoriteCoordinator.pushMovieDetailVC(id: 10)
+    
+    XCTAssertEqual(router.pushCallCount, 1)
+    XCTAssertEqual(coordinatorFactory.makeMovieDetailCoordinatorCallCount, 1)
+    XCTAssertEqual(screenFactory.makeMovieDetailScreenCallCount, 1)
+  }
+}
